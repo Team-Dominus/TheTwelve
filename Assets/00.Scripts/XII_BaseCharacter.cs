71e758b621ebefb617ee92d20c5acac60ba81fbd
youngeun9100 <youngeun8574@gmail.com>
2025-07-01 10:23:40 +0900

    Add : Folder
    Add 00.Scripts Folder

---
+++ Assets/00.Scripts/XII_BaseCharacter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 링크 / 담당자
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("XII_BaseCharacter Start");
+    }
+
+
+}
